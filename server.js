e108996eca13f52b12abe6cd5b4c0dfff9b3d130
aonurtaylan1-star <aonurtaylan1@gmail.com>
2026-01-02 19:52:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,13 @@
-const express = require('express');
-const { ExpressPeerServer } = require('peer');
-const path = require('path');
-const app = express();
-
-const server = app.listen(process.env.PORT || 3000);
-
-// PeerServer ayarları
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-    path: '/'
-});
-
-app.use('/peerjs', peerServer);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Odadaki kullanıcı sayılarını tutmak için basit bir nesne
-const rooms = {};
-
-peerServer.on('connection', (client) => {
-    // Yeni biri bağlandığında yapılacaklar (isteğe bağlı)
-});
-
-peerServer.on('disconnect', (client) => {
-    // Ayrıldığında oda listesinden düşme mantığı buraya eklenebilir
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-console.log("Sunucu modern modda çalışıyor...");
+{
+  "name": "konisma-web-app",
+  "version": "1.0.0",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.18.2",
+    "peer": "^1.0.1",
+    "socket.io": "^4.7.2"
+  },
+  "scripts": {
+    "start": "node server.js"
+  }
+}
