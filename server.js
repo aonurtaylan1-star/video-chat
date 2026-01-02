8b7220d00aedc1d56aecfd960e5307c476c04888
aonurtaylan1-star <aonurtaylan1@gmail.com>
2026-01-02 19:48:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
-const app = express();
 const path = require('path');
+const app = express();
 
 const server = app.listen(process.env.PORT || 3000);
 
+// PeerServer ayarları
 const peerServer = ExpressPeerServer(server, {
     debug: true,
-    path: '/myapp'
+    path: '/'
 });
 
 app.use('/peerjs', peerServer);
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Odadaki kullanıcı sayılarını tutmak için basit bir nesne
+const rooms = {};
+
+peerServer.on('connection', (client) => {
+    // Yeni biri bağlandığında yapılacaklar (isteğe bağlı)
+});
+
+peerServer.on('disconnect', (client) => {
+    // Ayrıldığında oda listesinden düşme mantığı buraya eklenebilir
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-console.log("Sunucu çalışıyor...");
+console.log("Sunucu modern modda çalışıyor...");
